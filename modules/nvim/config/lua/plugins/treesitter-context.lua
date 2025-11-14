f7f7da1aeb9ef8e0b65323de7e8e68426f7584fb
skyler-oakeson <skyler.oakeson@gmail.com>
2025-11-13 22:32:48 -0700

    Removed nvim config from repo.

--- modules/nvim/config/lua/plugins/treesitter-context.lua
+++
@@ -1,6 +0,0 @@
-vim.pack.add({ "https://github.com/nvim-treesitter/nvim-treesitter-context" })
-require('treesitter-context').setup({
-    enable = true,
-    max_lines = 3,
-    line_numbers = true
-})
