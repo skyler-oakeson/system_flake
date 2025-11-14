fc88d11749ca15585ac2a412c6afe15680942ac6
skyler-oakeson <skyler.oakeson@gmail.com>
2025-11-13 23:26:33 -0700

    Testing this.

--- packages/nvim.nix
+++ packages/nvim.nix
@@ -11,19 +11,19 @@ pkgs.symlinkJoin {
         --append-flags '-u ${inputs.nvim-config}/init.lua' \
         --prefix PATH : ${
           lib.makeBinPath [
-            nixd
-            nixfmt-rfc-style
-            csharp-ls
-            clang-tools
-            marksman
-            rustup
-            jdt-language-server
-            typescript-language-server
-            lua-language-server
-            texlab
-            csharp-ls
-            basedpyright
-            vscode-langservers-extracted
+            # nixd
+            # nixfmt-rfc-style
+            # csharp-ls
+            # clang-tools
+            # marksman
+            # rustup
+            # jdt-language-server
+            # typescript-language-server
+            # lua-language-server
+            # texlab
+            # csharp-ls
+            # basedpyright
+            # vscode-langservers-extracted
           ]
         }
   '';
