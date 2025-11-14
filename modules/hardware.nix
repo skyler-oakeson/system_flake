8c74f0a0af1cf0e4492172a4366a47744d0090fd
skyler-oakeson <skyler.oakeson@gmail.com>
2025-11-13 22:50:05 -0700

    Simplified hardware module.

--- modules/hardware.nix
+++ modules/hardware.nix
@@ -6,6 +6,43 @@
 }:
 let
   cfg = config.hardware.drivers;
+
+  amd = {
+    boot.initrd.kernelModules = [ "amdgpu" ];
+    environment.systemPackages = with pkgs; [ lact ];
+
+    # Enable for wayland and xorg
+    services.xserver.videoDrivers = [ "amdgpu" ];
+
+    # For 32 bit applications
+    hardware.graphics.enable32Bit = true;
+
+    # Start the gpu management deamon
+    systemd = {
+      packages = with pkgs; [ lact ];
+      services.lactd.wantedBy = [ "multi-user.target" ];
+    };
+  };
+
+  nvidia = {
+    hardware.graphics = {
+      enable = true;
+    };
+
+    services.xserver.videoDrivers = [ "nvidia" ];
+
+    hardware.nvidia = {
+      modesetting.enable = true;
+
+      # Helps with suspend and wake
+      powerManagement.enable = true;
+
+      open = false;
+      nvidiaSettings = true;
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+    };
+
+  };
 in
 {
   options = {
@@ -17,59 +54,8 @@ in
     };
   };
 
-  config =
-    { }
-    // lib.mkIf (cfg == "AMD") {
-      boot.initrd.kernelModules = [ "amdgpu" ];
-      environment.systemPackages = with pkgs; [ lact ];
-
-      # Enable for wayland and xorg
-      services.xserver.videoDrivers = [ "amdgpu" ];
-
-      # For 32 bit applications
-      hardware.graphics = {
-        enable32Bit = true; # For 32 bit applications
-        extraPackages32 = with pkgs; [
-          driversi686Linux.amdvlk
-        ];
-
-        # AMD vulkan drivers
-        extraPackages = with pkgs; [
-          amdvlk
-        ];
-      };
-
-      # Start the gpu management deamon
-      systemd = {
-        packages = with pkgs; [ lact ];
-        services.lactd.wantedBy = [ "multi-user.target" ];
-      };
-
-      # for Southern Islands (SI i.e. GCN 1) cards
-      # for Sea Islands (CIK i.e. GCN 2) cards
-      boot.kernelParams = [
-        "radeon.si_support=0"
-        "amdgpu.si_support=1"
-        "radeon.cik_support=0"
-        "amdgpu.cik_support=1"
-      ];
-    }
-    // lib.mkIf (cfg == "NVIDIA") {
-      hardware.graphics = {
-        enable = true;
-      };
-
-      services.xserver.videoDrivers = [ "nvidia" ];
-
-      hardware.nvidia = {
-        modesetting.enable = true;
-
-        # Helps with suspend and wake
-        powerManagement.enable = true;
-
-        open = false;
-        nvidiaSettings = true;
-        package = config.boot.kernelPackages.nvidiaPackages.stable;
-      };
-    };
+  config = lib.mkMerge [
+    (lib.mkIf (cfg == "AMD") amd)
+    (lib.mkIf (cfg == "NVIDIA") nvidia)
+  ];
 }
