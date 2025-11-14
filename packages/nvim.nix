b6439d504c4ec46b26673ae2a809716888dc13fb
skyler-oakeson <skyler.oakeson@gmail.com>
2025-11-13 23:27:09 -0700

    Testing this.

--- packages/nvim.nix
+++ packages/nvim.nix
@@ -8,23 +8,6 @@ pkgs.symlinkJoin {
   paths = [ inputs.neovim-nightly-overlay.packages.${pkgs.system}.default ];
   postBuild = with pkgs; ''
     wrapProgram $out/bin/nvim \
-        --append-flags '-u ${inputs.nvim-config}/init.lua' \
-        --prefix PATH : ${
-          lib.makeBinPath [
-            # nixd
-            # nixfmt-rfc-style
-            # csharp-ls
-            # clang-tools
-            # marksman
-            # rustup
-            # jdt-language-server
-            # typescript-language-server
-            # lua-language-server
-            # texlab
-            # csharp-ls
-            # basedpyright
-            # vscode-langservers-extracted
-          ]
-        }
+        --append-flags '-u ${inputs.nvim-config}/init.lua'
   '';
 }
